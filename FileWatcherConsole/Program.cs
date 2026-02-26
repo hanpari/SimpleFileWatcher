d4df028c4b51d1b307f62b49040f789b5331f457
hanpari <hanpari@outlook.com>
2026-02-26 19:42:41 +0100

    Create working example

--- FileWatcherConsole/Program.cs
+++ FileWatcherConsole/Program.cs
@@ -1,10 +1,18 @@
-﻿using static System.Console;
-using Hanpari.Monitor;
+﻿using Hanpari.Monitor;
+using static System.Console;
+
+var hub = new AsyncStreamHub<(FileInfo, Status)>();
+
+Hanpari.Monitor.Monitor m1 = new("test1");
+Hanpari.Monitor.Monitor m2 = new("test2");
+
+var id1 = hub.Add(m1.WatchEvery());
+var id2 = hub.Add(m2.WatchEvery());
 
 Clear();
 
-
-
-
-
-
+await foreach (var (f, status) in hub.Output)
+{
+    WriteLine(f);
+    WriteLine(status);
+}
